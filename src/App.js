91e16549dc7b0288765c213fffdbc6b58b096957
BibianaBalBar <balbuenabibi@gmail.com>
2020-12-09 19:06:04 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const location = axios.get("http://api.weatherapi.com/v1/current.json?key=96323f3867ef457f861184227200912&q=London")
+    .then(data => {
+      setWeather(data.data);
+    })
+    .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {weather && (
+        <h1>{weather.location.name}</h1>
+      )}      
     </div>
   );
 }
